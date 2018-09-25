9471ef61b12156c6806264c98af51ef836b74212
Toqeer <toqeerchaudhary01@gmail.com>
2018-09-25 17:11:39 +0500

    adding links to 3 other files

--- resources/views/includes/backend/script-files.blade.php
+++ resources/views/includes/backend/script-files.blade.php
@@ -1,34 +1,34 @@
 <!-- BEGIN PAGE LEVEL PLUGINS -->
 
-<script src="{{asset("assets/backend/plugins/jquery/jquery.min.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/jquery/jquery.min.js")}}"></script>
 
-<script src="{{asset("assets/backend/plugins/popper/popper.min.js")}}"></script>
-<script src="{{asset("assets/backend/plugins/responsejs/response.min.js")}}"></script>
-<script src="{{asset("assets/backend/plugins/loading-overlay/loadingoverlay.min.js")}}"></script>
-<script src="{{asset("assets/backend/plugins/tether/js/tether.min.js")}}"></script>
-<script src="{{asset("assets/backend/plugins/bootstrap/js/bootstrap.min.js")}}"></script>
-<script src="{{asset("assets/backend/plugins/jscrollpane/jquery.jscrollpane.min.js")}}"></script>
-<script src="{{asset("assets/backend/plugins/jscrollpane/jquery.mousewheel.js")}}"></script>
-<script src="{{asset("assets/backend/plugins/flexibility/flexibility.js")}}"></script>
-<script src="{{asset("assets/backend/plugins/noty/noty.min.js")}}"></script>
-<script src="{{asset("assets/backend/plugins/velocity/velocity.min.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/popper/popper.min.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/responsejs/response.min.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/loading-overlay/loadingoverlay.min.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/tether/js/tether.min.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/bootstrap/js/bootstrap.min.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/jscrollpane/jquery.jscrollpane.min.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/jscrollpane/jquery.mousewheel.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/flexibility/flexibility.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/noty/noty.min.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/velocity/velocity.min.js")}}"></script>
 <!-- END PAGE LEVEL PLUGINS -->
 {{--js tree--}}
-<script src="{{asset("assets/backend/plugins/jstree/jstree.min.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/jstree/jstree.min.js")}}"></script>
 <!-- BEGIN THEME LAYOUT SCRIPTS -->
-<script src="{{asset("assets/backend/js/common.min.js")}}"></script>
+<script src="{{secure_asset("assets/backend/js/common.min.js")}}"></script>
 <!-- END THEME LAYOUT SCRIPTS -->
 
 {{--sweet alert--}}
-<script src="{{asset("assets/backend/js/custom/sweetalert.min.js")}}"></script>
+<script src="{{secure_asset("assets/backend/js/custom/sweetalert.min.js")}}"></script>
 
 {{--converting the code into a javascript file and the type attribute's value is come with the template--}}
-{{--<script type="application/javascript" src="{{asset("assets/backend/js/application.js")}}"></script>--}}
+{{--<script type="application/javascript" src="{{secure_asset("assets/backend/js/application.js")}}"></script>--}}
 
 {{--For Custom Loaing--}}
 <script type="text/javascript" src="{{"assets/backend/plugins/Customizable-Loading-Modal-Plugin/js/modal-loading.js"}}"></script>
 <script type="text/javascript" src="//code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
-{{--<script src="{{asset("assets/backend/plugins/jquery-ui/jquery-ui.js")}}"></script>--}}
+{{--<script src="{{secure_asset("assets/backend/plugins/jquery-ui/jquery-ui.js")}}"></script>--}}
 
 {{--this yield is for a pages's special js file--}}
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
@@ -36,23 +36,23 @@
 @yield("scripts")
 
 {{--goal hack js--}}
-<script src="{{asset("assets/backend/js/goalhack.js")}}"></script>
+<script src="{{secure_asset("assets/backend/js/goalhack.js")}}"></script>
 
-<script src="{{asset("assets/backend/plugins/prism/prism.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/prism/prism.js")}}"></script>
 
 {{--for currency--}}
 {{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>--}}
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-maskmoney/3.0.2/jquery.maskMoney.min.js"></script>--}}
 {{--for contact--}}
 
-<script src="{{asset("assets/backend/plugins/telephone/js/intlTelInput.js")}}"></script>
-<script src="{{asset("assets/backend/plugins/telephone/js/utils.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/telephone/js/intlTelInput.js")}}"></script>
+<script src="{{secure_asset("assets/backend/plugins/telephone/js/utils.js")}}"></script>
 
-<script src="{{asset("assets/backend/js/moment.js")}}"></script>
+<script src="{{secure_asset("assets/backend/js/moment.js")}}"></script>
 
 {{--Pick Me Up Calender--}}
-<script src="{{asset("assets/backend/js/pickmeup.min.js")}}"></script>
-<script src="{{asset("assets/backend/js/jquery.pickmeup.uikit.js")}}"></script>
+<script src="{{secure_asset("assets/backend/js/pickmeup.min.js")}}"></script>
+<script src="{{secure_asset("assets/backend/js/jquery.pickmeup.uikit.js")}}"></script>
 
 {{--Geo Locaions CDNs--}}
 
--- resources/views/includes/frontend/script-files.blade.php
+++ resources/views/includes/frontend/script-files.blade.php
@@ -1,31 +1,31 @@
 <!-- jQuery -->
-<script src="{{asset("assets/frontend/js/jquery.min.js")}}"></script>
+<script src="{{secure_asset("assets/frontend/js/jquery.min.js")}}"></script>
 <!-- Bootstrap JS -->
-<script src="{{asset("assets/frontend/js/bootstrap.min.js")}}"></script>
+<script src="{{secure_asset("assets/frontend/js/bootstrap.min.js")}}"></script>
 <!-- Modernizr JS -->
-<script src="{{asset("assets/frontend/js/modernizr.custom.97074.js")}}"></script>
+<script src="{{secure_asset("assets/frontend/js/modernizr.custom.97074.js")}}"></script>
 <!-- Magnific Popup JS -->
-<script src="{{asset("assets/frontend/js/jquery.magnific-popup.min.js")}}"></script>
+<script src="{{secure_asset("assets/frontend/js/jquery.magnific-popup.min.js")}}"></script>
 <!-- OwlCarousel JS -->
-<script src="{{asset("assets/frontend/js/owl.carousel.min.js")}}"></script>
+<script src="{{secure_asset("assets/frontend/js/owl.carousel.min.js")}}"></script>
 <!-- SlickNav JS -->
-<script src="{{asset("assets/frontend/js/jquery.slicknav.min.js")}}"></script>
+<script src="{{secure_asset("assets/frontend/js/jquery.slicknav.min.js")}}"></script>
 <!-- Counter JS -->
-<script src="{{asset("assets/frontend/js/jquery.counterup.min.js")}}"></script>
+<script src="{{secure_asset("assets/frontend/js/jquery.counterup.min.js")}}"></script>
 <!-- Way Points JS -->
-<script src="{{asset("assets/frontend/js/waypoints-min.js")}}"></script>
+<script src="{{secure_asset("assets/frontend/js/waypoints-min.js")}}"></script>
 <!-- Hoverdir JS -->
-<script src="{{asset("assets/frontend/js/jquery.hoverdir.js")}}"></script>
+<script src="{{secure_asset("assets/frontend/js/jquery.hoverdir.js")}}"></script>
 <!-- Custom JS -->
-<script src="{{asset("assets/frontend/js/custom.js")}}"></script>
+<script src="{{secure_asset("assets/frontend/js/custom.js")}}"></script>
 <!-- GoalHack JS -->
-<script src="{{asset("assets/frontend/js/goalhack.js")}}"></script>
+<script src="{{secure_asset("assets/frontend/js/goalhack.js")}}"></script>
 
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <!-- Package Payment 1 -->
 {{--<script src="https://js.stripe.com/v2/"></script>--}}
 
-<script src="{{asset("assets/frontend/js/formValidation.min.js")}}"></script>
+<script src="{{secure_asset("assets/frontend/js/formValidation.min.js")}}"></script>
 
 {{--<script src="https://github.com/formvalidation/formvalidation.io/blob/master/vendor/formvalidation/js/framework/bootstrap.min.js"></script>--}}
 
@@ -36,7 +36,7 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.payment/1.2.3/jquery.payment.min.js"></script>
 
 <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
-<script src="{{asset("assets/frontend/js/country.js")}}"></script>
+<script src="{{secure_asset("assets/frontend/js/country.js")}}"></script>
 
 {{--For Custom Loaing--}}
 <script type="text/javascript" src="{{"assets/backend/plugins/Customizable-Loading-Modal-Plugin/js/modal-loading.js"}}"></script>
